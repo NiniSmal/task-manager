52c5ae073983b34f30a9a4ae0c98c66a0b6a0261
Nina Smolentseva <ninamusatova@gmail.com>
2024-03-03 16:21:18 +0300

    add input createTask

--- ui/index.js
+++ ui/index.js
@@ -9,42 +9,29 @@
 //     tasksUl.innerHTML =`<li>${task.name}: ${task.status}</li>`
 // })
 
-fetch('http://localhost:8021/getAllTasks',{}).
-then(response => response.json()).
-then(tasks => {
+fetch('http://localhost:8021/getAllTasks', {}).then(response => response.json()).then(tasks => {
     const tasksUl = document.getElementById('tasks')
-        tasks.forEach(function (tasks){
-        tasksUl.innerHTML +=`<li>${tasks.name}: ${tasks.status}</li>`
+    tasks.forEach(function (tasks) {
+        tasksUl.innerHTML += `<li>${tasks.name}: ${tasks.status}</li>`
     })
 })
-// fetch('http://localhost:8021//createTask', {}).
-// then(response =>response.json()).
-// then(creatTask =>{
-//     let inputElement = document.getElementById('creatTask');//получаем введенный элемент
-//     var value = inputElement.value;//получаем значение ,введенное пользователем
-// })
-// let inputElement = document.getElementById('creatTask');//получаем введенный элемент
-// var value = inputElement.value;//получаем значение ,введенное пользователем
+
+let button = document.getElementById('buttonCreate');
+button.addEventListener('click', () => {
+    let task = document.getElementById('nameTask').value
+    let body = {
+        name: task
+    }
+    let bodyJson = JSON.stringify(body)
+    fetch('http://localhost:8021/createTask', {
+        method: "POST",
+        body: bodyJson,
+    }).
+    then(response => {
+        console.log(response.status)
+        location.reload();
+    })
+});
 
 
-function createTask(){
-    let name =document.getElementById('nameTask').value;
-    let params =new URLSearchParams();
-    params.set('nameTask', name);
 
-    fetch('http://localhost:8021/createTask',{
-        method:'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body:params
-    }).then(
-        response =>{
-            return response.json();
-        }
-    ).then(
-        text =>{
-            document.getElementById('result').innerHTML =text;
-        }
-    )
-}
