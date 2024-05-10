affd10de76cb8890752c84b15902b4b1262ad7ab
ninasmolentseva <musatovanina90@gmail.com>
2024-05-10 13:51:57 +0300

    test projects

--- repository/project_repository_test.go
+++ repository/project_repository_test.go
@@ -165,5 +165,43 @@ func TestProjectRepository_UserProjects(t *testing.T) {
 	for _, project := range projects {
 		require.Contains(t, projectsDB, project)
 	}
+}
 
+func TestProjectRepository_Projects(t *testing.T) {
+	db, rds := ProjectConnection(t)
+	pr := NewProjectRepository(db, rds)
+	ur := NewUserRepository(db, rds)
+	ctx := context.Background()
+
+	user := entity.User{Email: uuid.NewString()}
+	userID, err := ur.CreateUser(ctx, user)
+	require.NoError(t, err)
+	user.ID = userID
+
+	createdAt := time.Now().Round(time.Millisecond).UTC()
+	projects := []entity.Project{
+		{
+			Name:      uuid.New().String(),
+			CreatedAt: createdAt,
+			UpdatedAt: createdAt,
+			UserID:    user.ID,
+		}, {
+			Name:      uuid.New().String(),
+			CreatedAt: createdAt,
+			UpdatedAt: createdAt,
+			UserID:    user.ID,
+		},
+	}
+	for i, project := range projects {
+		projectID, err := pr.SaveProject(ctx, project)
+		require.NoError(t, err)
+		projects[i].ID = projectID
+	}
+	filter := entity.ProjectFilter{UserID: user.ID}
+	projectsDB, err := pr.Projects(ctx, filter)
+	require.NoError(t, err)
+
+	for _, project := range projects {
+		require.Contains(t, projectsDB, project)
+	}
 }
